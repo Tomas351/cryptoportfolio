abb82d90deeed7dff1c15803bacac6b80c57a63a
Tomas <tomas.rasiukevicius@gmail.com>
2019-04-29 16:07:24 +0300

    installed chartjs, added name, description and current prices to portfolio view

--- src/viewPortfolio.js
+++ src/viewPortfolio.js
@@ -1,9 +1,77 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class List extends Component {
+    constructor(){
+        super()
+        this.state = {
+            portfolio_name: "",
+            description: "",
+            btc: "",
+            eth: "",
+            ltc: "",
+            ethprice : "",
+            btcprice : "",
+            ltcprice : ""
+        }
+    }
+
+componentDidMount(){
+    axios.get('http://localhost:4000/portfolio/edit/'+this.props.match.params.id)
+            .then(response => {
+                this.setState({ 
+                    portfolio_name: response.data.portfolio_name, 
+                    description: response.data.description,
+                    btc: response.data.btc,
+                    eth: response.data.eth,
+                    ltc: response.data.ltc
+                })
+            })
+            .then(axios.get('https://api.coinranking.com/v1/public/coins?ids=1,2,7')
+            .then(response => {
+                this.setState({ 
+                    btcprice: response.data.data.coins[0].price,
+                    ethprice: response.data.data.coins[1].price,
+                    ltcprice: response.data.data.coins[2].price
+                } )
+            }))
+            .catch(function (error) {
+                console.log(error)
+            })
+}
     render() {
         return (
-            <h1>portfolio stuff goes here</h1>
+        <div align="center">
+           <h1>{this.state.portfolio_name} portfolio</h1>
+           <br />
+           <p>{this.state.description}</p>
+           <br />
+           <h2>Current crypto currency price:</h2>
+               <table className="table table-bordered" style={{ marginTop: 20 }}>
+              <thead>
+                <tr>
+                  <th>Coin</th>
+                  <th>Price ($)</th>
+                </tr>
+              </thead>
+              <tbody>
+      <tr>
+        <td>BTC</td>
+        <td>{this.state.btcprice}</td>
+      </tr>
+      <tr>
+        <td>ETH</td>
+        <td>{this.state.ethprice}</td>
+      </tr>
+      <tr>
+        <td>LTC</td>
+        <td>{this.state.ltcprice}</td>
+      </tr>
+              </tbody>
+            </table>
+
+            
+           </div>
         )
     }
 }
