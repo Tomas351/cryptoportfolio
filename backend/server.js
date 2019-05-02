b03e4bc28c589a398635b0518704d54567b10ece
Tomas <tomas.rasiukevicius@gmail.com>
2019-05-02 16:22:08 +0300

    heroku fix
    api works now, but doesnt load web

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,9 @@ app.use('/api', portfolioRoute);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'))
+  })
 }
 
 app.listen(PORT, function(){
